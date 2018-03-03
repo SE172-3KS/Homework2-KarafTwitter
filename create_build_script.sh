d7b5410d89822ea3b9f37be9cebf3d2a0cf96f15
haeol <k.tom_1@yahoo.com>
2018-03-03 11:00:20 -0800

    remove trailing \n from build script creation

--- create_build_script.sh
+++ create_build_script.sh
@@ -9,7 +9,7 @@ fi
 echo "Enter path to apache deploy folder: "
 read folder
 
-echo "#!/usr/bin/env bash\n" >> ${file}
+echo "#!/usr/bin/env bash" >> ${file}
 echo "mvn clean install" >> ${file}
 echo "mv ./target/twitter-api-3ks-1.0.0.war ${folder}" >> ${file}
 echo "Created ${file} in current directory"
